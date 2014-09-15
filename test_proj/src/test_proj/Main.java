00b2c2a01f6007a379d7da3cfd5cb3c82b6a1f50
garg <dipti.garg@siemens.com>
2014-09-15 15:04:58 +0530

    first change to output

--- test_proj/src/test_proj/Main.java
+++ test_proj/src/test_proj/Main.java
@@ -13,6 +13,7 @@ public class Main
     private static void displayGreeting()
     {
         System.out.println("HI...");
+        System.out.println("******");
         System.out.println("Welcome here...");
     }
 }
