c99e2dfd0165a22cd282801c96de6bf6e05ac40d
IC018059 <IC018059@AAEINGN500734D.ww888.siemens.net>
2014-09-16 15:57:18 +0530

    eigth commit

--- test_proj/src/test_proj/Main.java
+++ test_proj/src/test_proj/Main.java
@@ -21,5 +21,7 @@ public class Main
         System.out.println("test3");
         System.out.println("test4");
         System.out.println("test5");
+        
+        System.out.println("test6");
     }
 }
